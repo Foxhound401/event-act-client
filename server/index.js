abe391b0791093389388dc06073d51bfbfcbe342
Tung <vmtung93@gmail.com>
2019-05-26 19:52:08 +0700

    add node js express server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+// load .env using dotenv first
+const dotenv = require('dotenv')
+
+// include other main deps
+const express = require('express')
+const bodyParser = require('body-parser')
+const compression = require('compression')
+
+dotenv.config({ silent: true })
+
+// instantiate express
+const app = express()
+const PRODUCTION = process.env.NODE_ENV === 'production'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(compression())
+
+app.use(express.static(`${__dirname}/../dist`))
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: `${__dirname}/../dist` })
+})
+
+const serverPort = process.env.PORT || 3000
+app.listen(serverPort)
+console.log(
+  `Express server @ http://localhost:${serverPort} (${
+    PRODUCTION ? 'production' : 'development'
+  })\n`
+)
